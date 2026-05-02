234d46399763ae470ec791cacb3dd620129dfe95
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2026-05-02 20:05:57 +0300

    implementation views(home,about) and HomeController

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,1 +1,11 @@
 <?php
+    class HomeController{
+        public function index() {
+            echo "<h1>Home Page </h1>";
+        }
+
+        public function about(){
+            echo "<h1>About Page </h1>";
+        }
+
+    }
